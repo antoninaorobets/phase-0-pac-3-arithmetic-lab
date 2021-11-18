011efb890f15a62258cc4966033cfe613235440b
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-18 16:15:32 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(num1, num2) {
+    return num1+num2
+}
+
+function subtract(num1, num2) {
+    return num1-num2
+}
+function multiply(num1, num2) {
+    return num1*num2
+}
+function divide(num1, num2) {
+    return num1/num2
+}
+
+function increment(num1) {
+    num1++
+    return num1
+}
+
+function decrement(num1) {
+    num1--
+    return num1
+} 
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n,10)
+}
